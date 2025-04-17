33b12196389ffd185fcd5af410bdb5f09f0870d0
qminh011002 <qminh.workmode@gmail.com>
2025-04-17 10:35:23 +0700

    Update

--- structs.go
+++ structs.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+// Structs are defined using the 'type' and 'struct' keywords followed by
+// curly braces '{}'
+// Fields are defined with a name and a type
+
+func main() {
+	type Person struct {
+		firstName string
+		lastName  string
+		age       int
+	}
+
+	p := Person{
+		firstName: "John",
+		lastName:  "Doe",
+		age:       23,
+	}
+
+	fmt.Println(p)
 }
